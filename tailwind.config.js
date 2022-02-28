07480362b407c82f1d13eca2420c3f7bd37c4c22
Kunal Mathur <dev.kunalmathur@gmail.com>
2022-02-28 22:32:06 +0530

    get github repos

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+  content: ["./pages/**/*.{html,js,jsx}", "./components/**/*.{html,js,jsx}"],
+  darkMode: "media" /* media, false */,
+  theme: {
+    extend: {
+      colors: {
+        primary: colors.blue,
+      },
+    },
+  },
+  plugins: [],
+};
