574486f4cf8c1340365440a460cc7920cdf08001
iKunalmathur <iKunalmathur@users.noreply.github.com>
2022-03-02 15:32:09 +0000

    Deploying to gh-pages from @ iKunalmathur/ikunalmathur.github.io@f48d440b11c7af257a8b18526ac3a2d436fc062f 🚀

--- _next/static/hnR7yFosnpNAStJVSuxtf/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-ab05d5dc01ad4a1f.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
